ca0c9ad48d36a4b9d16264910faca320d7c898df
seescode <seesyong@gmail.com>
2016-01-21 15:41:31 -0500

    super alert text

--- dist/directives/superAlert/superAlert.js
+++ dist/directives/superAlert/superAlert.js
@@ -11,7 +11,7 @@
             templateUrl: '/directives/superAlert/superAlert.html',
             link: function (scope, element, attrs) {
                 scope.click = function() {
-                    alert('abcd');
+                    alert('super alert');
                 };
             }
         };
--- sample/superAlert.js
+++ sample/superAlert.js
@@ -26477,7 +26477,7 @@ angular.module('atom.superalert').run(['$templateCache', function($templateCache
             templateUrl: '/directives/superAlert/superAlert.html',
             link: function (scope, element, attrs) {
                 scope.click = function() {
-                    alert('abcd');
+                    alert('super alert');
                 };
             }
         };
