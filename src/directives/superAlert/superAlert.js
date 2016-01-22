076f122461ee0065c62dbf374fc3b5df19573c05
seescode <seesyong@gmail.com>
2016-01-22 10:14:06 -0500

    update

--- src/directives/superAlert/superAlert.js
+++ src/directives/superAlert/superAlert.js
@@ -11,7 +11,7 @@
             templateUrl: '/directives/superAlert/superAlert.html',
             link: function (scope, element, attrs) {
                 scope.click = function() {
-                    alert('super alert 2');
+                    alert('super alert 3');
                 };
             }
         };
